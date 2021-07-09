383503ac9275e0f3c2f444505d1ad2011276191c
tscz <tscz@users.noreply.github.com>
2021-07-09 22:43:27 +0200

    Add react+tailwind

---
+++ react_tailwind/src/App.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid grid-cols-12 gap-4 h-screen">
+      <div className="bg-white rounded-xl shadow-md col-span-3">1</div>
+      <div className="bg-white rounded-xl shadow-md">2</div>
+      <div className="bg-white rounded-xl shadow-md">3</div>
+      <div className="bg-white rounded-xl shadow-md col-span-2 container">
+        <img className="object-center" src={logo} alt="logo"></img>
+      </div>
+      <div className="bg-white rounded-xl shadow-md">5</div>
+      <div className="bg-white rounded-xl shadow-md">6</div>
+      <div className="bg-white rounded-xl shadow-md">7</div>
+      <div className="bg-white rounded-xl shadow-md">8</div>
+      <div className="bg-white rounded-xl shadow-md">9</div>
+      <div className="bg-white rounded-xl shadow-md">10</div>
+      <div className="bg-white rounded-xl shadow-md">11</div>
+      <div className="bg-white rounded-xl shadow-md">12</div>
+      <div className="bg-white rounded-xl shadow-md">13</div>
+      <div className="bg-white rounded-xl shadow-md">14</div>
+      <div className="bg-white rounded-xl shadow-md">15</div>
+      <div className="bg-white rounded-xl shadow-md">16</div>
+      <div className="bg-white rounded-xl shadow-md">17</div>
+      <div className="bg-white rounded-xl shadow-md">18</div>
+      <div className="bg-white rounded-xl shadow-md">19</div>
+      <div className="bg-white rounded-xl shadow-md">20</div>
+      <div className="bg-white rounded-xl shadow-md">21</div>
+      <div className="bg-white rounded-xl shadow-md">22</div>
+      <div className="bg-white rounded-xl shadow-md">23</div>
+      <div className="bg-white rounded-xl shadow-md">24</div>
+      <div className="bg-white rounded-xl shadow-md">25</div>
+      <div className="bg-white rounded-xl shadow-md">26</div>
+      <div className="bg-white rounded-xl shadow-md">27</div>
+      <div className="bg-white rounded-xl shadow-md">28</div>
+      <div className="bg-white rounded-xl shadow-md">29</div>
+      <div className="bg-white rounded-xl shadow-md">30</div>
+      <div className="bg-white rounded-xl shadow-md">31</div>
+      <div className="bg-white rounded-xl shadow-md">32</div>
+      <div className="bg-white rounded-xl shadow-md">33</div>
+      <div className="bg-white rounded-xl shadow-md">34</div>
+      <div className="bg-white rounded-xl shadow-md">35</div>
+      <div className="bg-white rounded-xl shadow-md">36</div>
+      <div className="bg-white rounded-xl shadow-md">37</div>
+      <div className="bg-white rounded-xl shadow-md">38</div>
+      <div className="bg-white rounded-xl shadow-md">39</div>
+    </div>
+  );
+}
+
+export default App;
